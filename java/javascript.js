d1a41ca7595161922fec3997436b09b9c9e43001
JLeandroS <105645222+JLeandroS@users.noreply.github.com>
2025-08-20 17:18:07 -0300

    Update javascript.js

--- java/javascript.js
+++ java/javascript.js
@@ -268,6 +268,20 @@ intervalo = setInterval(() => {
 
 }, 100);
 
+const handlePhone = (event) => {
+  let input = event.target
+  input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
+
 // window.addEventListener('scroll', function() {
 //   console.log(Math.round(window.scrollY));
 // });
+
